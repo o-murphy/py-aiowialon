ac7d79af803689d812759ec37465c5974b2a6b69
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-03-05 11:15:36 +0200

    Package's build system update (#1)
    * switch to pyproject.toml
    * workflows scripts created

--- setup.py
+++ setup.py
@@ -1,39 +1,5 @@
 #!/usr/bin/env python
-import sys
-import aiowialon
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-extra = {}
-if sys.version_info >= (3,):
-    extra['use_2to3'] = True
-    extra['convert_2to3_doctests'] = ['README.md']
-
-CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
-
-KEYWORDS = 'async wialon remote api wrapper'
-
-setup(name='py-aiowialon',
-      version=aiowialon.__version__,
-      description="""Wialon Remote API wrapper for Python.""",
-      long_description=open('README.md').read(),
-      long_description_content_type="text/markdown",
-      author=aiowialon.__author__,
-      url='https://github.com/o-murphy/py-aiowialon',
-      packages=find_packages(),
-      download_url='http://pypi.python.org/pypi/py-aiowialon/',
-      classifiers=CLASSIFIERS,
-      keywords=KEYWORDS,
-      zip_safe=True,
-      install_requires=['aiohttp', 'simplejson', 'future'],
-      py_modules=['aiowialon']
-      )
+setup()
