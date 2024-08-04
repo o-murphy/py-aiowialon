e114aa76f1abee92e70766eda8e7fc2ec3911b5b
o-murphy <thehelixpg@gmail.com>
2024-08-04 15:58:46 +0300

    trying to create generic types

--- test/generics.py
+++ test/generics.py
@@ -1,24 +1,40 @@
 from dataclasses import dataclass
-from typing import TypedDict, List, Optional
+from functools import wraps
+from typing import TypedDict, List, Optional, Union
 
 from aiowialon.types.flags import AccessControlFlags, GetAccountResultType
 from aiowialon.compatibility import Unpack
-from strong_typing.serialization import json_to_object
+from strong_typing.serialization import json_to_object, object_to_json
 
 
-@dataclass
-class CoreGetAccountData:
+# @warp
+@dataclass(kw_only=True)
+class JsonRequestData:
+    class Params(TypedDict, total=False):
+        # abstract Params(TypedDict)
+        pass
+
+    def __post_init__(self):
+        # Initialize the instance using json_to_object if needed
+        params = self.__dict__
+        updated_instance = json_to_object(self.__class__, params)
+        # Update the current instance's attributes
+        self.__dict__.update(updated_instance.__dict__)
+
+    def json(self):
+        return object_to_json(self)
+
+# @warp
+@dataclass(kw_only=True)
+class CoreGetAccountData(JsonRequestData):
     type: GetAccountResultType = GetAccountResultType.MINIMAL
 
     class Params(TypedDict, total=False):
         type: GetAccountResultType
 
-    def __new__(cls, **kwargs: Unpack[Params]):
-        return json_to_object(cls, kwargs)
-
-
-@dataclass
-class CoreCheckItemsBilling:
+# @warp
+@dataclass(kw_only=True)
+class CoreCheckItemsBilling(JsonRequestData):
     items: List[int]
     accessFlags: AccessControlFlags
     serviceName: str
@@ -28,8 +44,6 @@ class CoreCheckItemsBilling:
         accessFlags: AccessControlFlags
         serviceName: str
 
-    def __new__(cls, **kwargs: Unpack[Params]):
-        return json_to_object(cls, kwargs)
 
 
 @dataclass
@@ -53,3 +67,14 @@ class TSearchAction:
 
 class SearchRequest(TypedDict):
     action: TSearchAction
+
+
+def func(**kwargs: Unpack[CoreGetAccountData.Params]):
+    obj = CoreGetAccountData(**kwargs)
+    print(obj)
+    json_data = obj.json()
+    print(json_data)
+
+
+func(type=2)
+print(json_to_object(CoreGetAccountData, {}))
