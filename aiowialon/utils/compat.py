6e53a870650bd2e8c4fd8b6df2d98c688a4eba98
o-murphy <thehelixpg@gmail.com>
2024-08-13 00:13:50 +0300

    updated test.yml

--- aiowialon/utils/compat.py
+++ aiowialon/utils/compat.py
@@ -2,14 +2,16 @@
 Compatibility module to allow using python <3.12
 """
 
-# pylint: skip-file
+# pylint: skip-file,disable-all
+
 
 import sys
+
 if sys.version_info[:2] < (3, 11):
-    from typing import Unpack
+    from typing_extensions import Unpack
     from strenum import StrEnum
 else:
-    from typing_extensions import Unpack
+    from typing import Unpack
     from enum import StrEnum
 
 __all__ = (
